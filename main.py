fca057180382d8988ee3dadd15d3e01de9eb97ae
Filippo Ferrando Damillano <filippo.ferrando@itiscuneo.eu>
2022-05-16 12:34:01 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+def Add(a, b):
+  return a + b
+  
+def Mul(a, b):
+  return a * b
+  
+def Div(a, b):
+  return a / b
+  
+def Sub(a, b):
+  return a - b
+        
+def hi():
+  print("Hi!")
+
+if __name__ == '__main__':
+  hi()
